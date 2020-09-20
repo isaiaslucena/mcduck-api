ee5d75b62cd88607ac85576ec62f4fe01caf968b
Isaias Neto <isaiasneto@gmail.com>
2020-09-19 23:56:45 -0300

    Created Log model

---
+++ app/Log.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model {
+	use HasFactory;
+
+	protected $logTable = 'log';
+}
